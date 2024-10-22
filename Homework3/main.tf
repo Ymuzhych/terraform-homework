42fb8bfcdea009dc69bc38abfbecb41a6b81b778
Ymuzhych <myuliia2018@gmail.com>
2024-10-22 01:22:21 +0000

    homework3

--- Homework3/main.tf
+++ Homework3/main.tf
@@ -2,7 +2,21 @@ resource "aws_key_pair" "tfhomework" {
   key_name   = "homeworktf"
   public_key = file("~/.ssh/id_rsa.pub")
 }
+  data "aws_ami" "linux2" {
+  most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"]
+  }
 resource "aws_instance" "instance1" {
   ami = data.aws_ami.linux2.id
   instance_type = "t2.micro"
@@ -39,19 +53,3 @@ resource "aws_instance" "instance3" {
   Name = "web3"
   }
 }
-  
-  data "aws_ami" "linux2" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["137112412989"]
-  }
